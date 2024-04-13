9946f987e02b077d307fd0cc38b1617c141e6942
Thiago <thiagobritto.developer@gmail.com>
2024-04-13 20:11:53 -0300

    add images

--- sanzia/database/db_produtos.js
+++ sanzia/database/db_produtos.js
@@ -2,13 +2,13 @@ export default {
     "Hambúrgueres": [
         {
             title: "Básico",
-            price: "7.00",
+            price: "8.00",
             desc: "Carne, ovo, queijo e presunto.",
             img: "assets/images/small/hamburger.png",
         },
         {
             title: "Completo",
-            price: "8.00",
+            price: "10.00",
             desc: "Carne, ovo, queijo, presunto, alface, tomate e batata palha.",
             img: "assets/images/small/hamburger.png",
         },
@@ -16,6 +16,12 @@ export default {
             title: "Duplo",
             price: "12.00",
             desc: "2 Carnes, 2 ovos, queijo x2, presunto x2, alface x2, tomate e batata palha em dobro.",
+            img: "assets/images/small/hamburger.png",
+        },
+        {
+            title: "Triplo",
+            price: "16.00",
+            desc: "3 Carnes, 3 ovos, queijo x3, presunto x3, alface x3, tomate e batata palha em triplo.",
             img: "assets/images/small/hamburger.png",
         },
         {
@@ -50,22 +56,177 @@ export default {
         },
     ],
 
-    bebidas: [
+    salgados: [
         {
-            title: "Coca-cola lata",
-            price: "4.00",
-            desc: "lkedfokiefrngikqajufehojbdfpio   WNFJBPDOFJL",
-            img: "https://www.designi.com.br/images/preview/10000392.jpg",
+            title: "Salgados",
+            price: "3.50",
+            desc: "Pastel, Coxinha, Risole",
+            img: "assets/images/small/salgados.png",
         },
         {
-            title: "Coca-cola lata",
+            title: "Panqueca",
             price: "4.00",
-            desc: "lkedfokiefrngikqajufehojbdfpio   WNFJBPDOFJL",
-            img: "https://www.designi.com.br/images/preview/10000392.jpg",
+            desc: "",
+            img: "assets/images/small/panqueca.png",
+        },
+        {
+            title: "Mini pizza",
+            price: "5.00",
+            desc: "",
+            img: "assets/images/small/mini_pizza.png",
         },
     ],
 
-    combos: [
+    sucos:[
+        {
+            title: "Suco 400ml",
+            price: "4.00",
+            desc: "shcol, itaipava, schin",
+            img: "assets/images/small/sucos.png",
+        },
+    ],
 
+    Refrigerantes: [
+        {
+            title: "Coca-cola lata",
+            price: "5.00",
+            desc: "",
+            img: "assets/images/small/coca_cola_lata.png",
+        },
+        {
+            title: "Fanta laranja lata",
+            price: "5.00",
+            desc: "",
+            img: "assets/images/small/fanta_laranja_lata.png",
+        },
+        {
+            title: "Fanta uva lata",
+            price: "5.00",
+            desc: "",
+            img: "assets/images/small/fanta_uva_lata.png",
+        },
+        {
+            title: "Sprite lata",
+            price: "5.00",
+            desc: "",
+            img: "assets/images/small/sprite_lata.png",
+        },
+        {
+            title: "Guaraná lata",
+            price: "5.00",
+            desc: "",
+            img: "assets/images/small/guarana_lata.png",
+        },
+        {
+            title: "Coca-cola 250ml ",
+            price: "3.00",
+            desc: "",
+            img: "assets/images/small/coca_cola_250ml.png",
+        },
+        {
+            title: "Fanta laranja 250ml ",
+            price: "3.00",
+            desc: "Guaraná",
+            img: "assets/images/small/fanta_laranja_250ml.png",
+        },
+        {
+            title: "Fanta uva 250ml ",
+            price: "3.00",
+            desc: "Coca, Fanta, Guaraná",
+            img: "assets/images/small/fanta_uva_250ml.png",
+        },
+        {
+            title: "Sprite 250ml ",
+            price: "3.00",
+            desc: "",
+            img: "assets/images/small/sprite_250ml.png",
+        },
+        {
+            title: "Guaraná 250ml ",
+            price: "3.00",
+            desc: "",
+            img: "assets/images/small/guarana_250ml.png",
+        },
+        {
+            title: "Coca-cola 1L",
+            price: "10.00",
+            desc: "Coca, Fanta, Guaraná",
+            img: "assets/images/small/coca_cola_1l.png",
+        },
+        {
+            title: "Fanta laranja 1L",
+            price: "10.00",
+            desc: "Coca, Fanta, Guaraná",
+            img: "assets/images/small/fanta_laranja_1l.png",
+        },
+        {
+            title: "Fanta uva 1L",
+            price: "10.00",
+            desc: "Coca, Fanta, Guaraná",
+            img: "assets/images/small/fanta_uva_1l.png",
+        },
+        {
+            title: "Sprite 1L",
+            price: "10.00",
+            desc: "",
+            img: "assets/images/small/sprite_1l.png",
+        },
+        {
+            title: "Guaraná 1L ",
+            price: "10.00",
+            desc: "",
+            img: "assets/images/small/guarana_1l.png",
+        },
+        {
+            title: "Coca-cola 2L",
+            price: "12.00",
+            desc: "Coca, Fanta, Guaraná",
+            img: "assets/images/small/coca_cola_2l.png",
+        },
+        {
+            title: "Fanta laranja 2L",
+            price: "12.00",
+            desc: "Coca, Fanta, Guaraná",
+            img: "assets/images/small/fanta_laranja_2l.png",
+        },
+        {
+            title: "Fanta uva 2L",
+            price: "12.00",
+            desc: "Coca, Fanta, Guaraná",
+            img: "assets/images/small/fanta_uva_2l.png",
+        },
+        {
+            title: "Sprite 2L",
+            price: "12.00",
+            desc: "",
+            img: "assets/images/small/sprite_2l.png",
+        },
+        {   
+            title: "Guaraná 1L ",
+            price: "10.00",
+            desc: "",
+            img: "assets/images/small/guarana_2l.png",
+        },
+    ],
+
+    bebidas: [
+        {
+            title: "Heineken lognet",
+            price: "10.00",
+            desc: "",
+            img: "assets/images/small/heineken_lognet.png",
+        },
+        {
+            title: "Budweiser lognet",
+            price: "10.00",
+            desc: "",
+            img: "assets/images/small/budweiser_lognet.png",
+        },
+        {
+            title: "Cerveja skol lata",
+            price: "5.00",
+            desc: "",
+            img: "assets/images/small/skol_lata.png",
+        },
     ],
 }
